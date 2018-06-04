c705c99c73b13391668c38608c32302d4fb259b2
Landers1037 <32225052+Landers1037@users.noreply.github.com>
2018-06-04 09:36:22 +0800

    Update 串行IO端口UART.c

--- 串行IO端口UART.c
+++ 串行IO端口UART.c
@@ -20,7 +20,7 @@
 #define intc_base 0x41200000
 #define uart_base 0x40600000
 
-void MY_ISR(void)_attribute_((interrupt_handler));//intr reg
+void My_ISR(void)__attribute__((interrupt_handler));
 void Initialize();
 void Uarthandler();
 void delay_50ms();
@@ -36,67 +36,69 @@ int main(void)
 		/*if(Rx_flag)
 		{xil_printf("the rx data is0x%x\r\n",rxData);
 		Rx_flag=0;
-		
+
 	}*/
 	delay_50ms();
 	txtime=txtime+1;
 	if(txtime==10000)txtime=0x00;
 		int uart_flag;
-		uart_flag=Xil_In32)(uart_base+uart_stat);
+		uart_flag=Xil_In32(uart_base+uart_stat);
 		if(uart_flag&0x04)
 			{
 				if(txtime%2)
 					{int txdata;
 						txdata=Xil_In32(gpio_base+sw_data);
-						xil_Out32(uart_base+uart_tx,xtdata);
+						Xil_Out32(uart_base+uart_tx,txdata);
 						}
 			}
 }
-return0;
+return 0;
 }
-void Intalize()
+void Initialize()
  {
  	Rx_flag=0x00;
  	rxData=0x00;
  	txtime=0x00;
  	Xil_Out8(gpio_base+sw_tri,0xff);
- 	xil_Out8(gpio_base+led_data,0x5a);
- 	xil_Out32(uart_base+uart_ctl,0x13);
- 	xil_Out32(intc_base+intc_iar,0xffffffff);
- 	xil_Out32(intc_base+intc_ier,0x00000001);
-  xil_Out32(intc_base+intc_mer,0x03);
-  microblaze_enable_interrpts();
-  
- 	
+ 	Xil_Out8(gpio_base+led_data,0x5a);
+ 	Xil_Out32(uart_base+uart_ctl,0x13);
+ 	Xil_Out32(intc_base+intc_iar,0xffffffff);
+ 	Xil_Out32(intc_base+intc_ier,0x00000001);
+    Xil_Out32(intc_base+intc_mer,0x03);
+    microblaze_enable_interrupts();
+
+
  }
+void My_ISR()
+{
  int status;
  status=Xil_In32(intc_base+intc_isr);
  if(status&0x01)
  	{
  		Uarthandler();
- 		
+
  	}
  	Xil_Out32(intc_base+intc_iar,status);
 }
 
 void Uarthandler()
-{
-	int uart_flag=Xil_In32(uart_base+uart_stat);
+{   int uart_flag;
+    uart_flag=Xil_In32(uart_base+uart_stat);
 	if(uart_flag&0x01)
 		{
 			Rx_flag=1;
-			rx_Data=Xil_In32(uart_base+urat_rx);
+			rxData=Xil_In32(uart_base+uart_rx);
 			xil_printf("0x%x\n\r",rxData);
 			Xil_Out8(gpio_base+led_data,rxData);
-			
+
 		}
 		Xil_Out32(uart_base+uart_ctl,0x13);
-		
+
 }
 
-void delay_50ms();
+void delay_50ms()
 {
 	int i;
 	for(i=0;i<50000000;i++);
-	
+
 }
